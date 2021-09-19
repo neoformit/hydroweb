de5549535d864e0c5ff2e4562c7959f5028c7432
Cameron Hyde <c.hyde@qcif.edu.au>
2021-09-19 18:01:15 +1000

    Add Measurement model

--- dash/models.py
+++ dash/models.py
@@ -1,51 +1,65 @@
 """Dashboard models."""
 
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Config(models.Model):
-    """Store live configuration to be updated by user."""
+    """Store live configuration to be updated by user.
 
     # Timing
-    SWEEP_CYCLE_MINUTES = models.IntegerField(null=True)
-    MIST_CYCLE_MINUTES = models.IntegerField(null=True)
-    MIST_DURATION_SECONDS = models.IntegerField(null=True)
-    MIX_PUMP_SECONDS = models.IntegerField(null=True)
-    MIX_ADDITION_DELAY_SECONDS = models.IntegerField(null=True)
-    WATER_FILL_INTERVAL_SECONDS = models.IntegerField(null=True)
-    WATER_FILL_MAX_SECONDS = models.IntegerField(null=True)
-    MEDIAN_SAMPLE_DELAY_SECONDS = models.FloatField(null=True)
-    QUIET_TIME_START: models.TimeField(null=True)
-    QUIET_TIME_END: models.TimeField(null=True)
+    SWEEP_CYCLE_MINUTES
+    MIST_CYCLE_MINUTES
+    MIST_DURATION_SECONDS
+    MIX_PUMP_SECONDS
+    MIX_ADDITION_DELAY_SECONDS
+    WATER_FILL_INTERVAL_SECONDS
+    WATER_FILL_MAX_SECONDS
+    MEDIAN_SAMPLE_DELAY_SECONDS
+    QUIET_TIME_START
+    QUIET_TIME_END
 
     # Pressure monitoring
-    MIN_PRESSURE_PSI = models.IntegerField(null=True)
-    MAX_PRESSURE_PSI = models.IntegerField(null=True)
-    ALERT_PRESSURE_PSI = models.IntegerField(null=True)
+    MIN_PRESSURE_PSI
+    MAX_PRESSURE_PSI
+    ALERT_PRESSURE_PSI
 
     # PH monitoring
-    PH_TARGET = models.FloatField(null=True)
-    PH_LIMIT_WARN = models.FloatField(null=True)
-    PH_LIMIT_DANGER = models.FloatField(null=True)
-    PH_ACTION_THRESHOLD = models.FloatField(null=True)
-    PH_ACTION_THRESHOLD = models.FloatField(null=True)
+    PH_TARGET
+    PH_LIMIT_WARN
+    PH_LIMIT_DANGER
+    PH_ACTION_THRESHOLD
+    PH_ACTION_THRESHOLD
 
     # EC monitoring
-    EC_TARGET = models.FloatField(null=True)
-    EC_LIMIT_WARN = models.FloatField(null=True)
-    EC_LIMIT_DANGER = models.FloatField(null=True)
-    EC_ACTION_THRESHOLD = models.FloatField(null=True)
-    EC_ADDITION_SECONDS = models.FloatField(null=True)
+    EC_TARGET
+    EC_LIMIT_WARN
+    EC_LIMIT_DANGER
+    EC_ACTION_THRESHOLD
+    EC_ADDITION_SECONDS
 
     # Water level monitoring
-    TANK_HEIGHT_MM = models.IntegerField(null=True)
-    DEPTH_MAXIMUM_MM = models.IntegerField(null=True)
-    DEPTH_LIMIT_WARN = models.FloatField(null=True)
-    DEPTH_LIMIT_DANGER = models.FloatField(null=True)
-    DEPTH_ACTION_THRESHOLD = models.FloatField(null=True)
+    TANK_HEIGHT_MM
+    DEPTH_MAXIMUM_MM
+    DEPTH_LIMIT_WARN
+    DEPTH_LIMIT_DANGER
+    DEPTH_ACTION_THRESHOLD
+    """
+
+    key = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
 
 
-class Monitor(models.Model):
+class Measurement(models.Model):
     """Store status monitoring data."""
 
-    pass
+    ph = models.FloatField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)])
+    ec = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)])
+    depth_mm = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(700)])
+    temp_c = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(40)])
+    pressure_psi = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(170)])

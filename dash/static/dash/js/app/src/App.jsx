5c73a0a09fbbd69f0c3c794f23542b1972ba9d71
Cameron Hyde <c.hyde@qcif.edu.au>
2021-09-19 18:55:15 +1000

    Render react app homepage

--- dash/static/dash/js/app/src/App.jsx
+++ dash/static/dash/js/app/src/App.jsx
@@ -2,19 +2,33 @@ import React from 'react'
 import ReactTooltip from 'react-tooltip';
 
 import ErrorBoundary from './utils/ErrorBoundary'
+import Logo from './assets/img/logo.png';
+import Logout from './assets/icons/logout.svg';
 
 
 const App = (props) => {
 
   return (
     <ErrorBoundary>
-      <div className="container">
+      <div className="main">
+        <div className="navbar">
 
-        // App content here
+          <div className="row px-5">
+            <img src={Logo} alt="HydroPi logo" />
+            <h1 className="title"> HydroPi </h1>
+          </div>
 
+          <a href="/logout">
+            <img className="icon" src={Logout} alt="HydroPi logo" width="50px" />
+          </a>
+        </div>
+
+        <div className="container text-center my-auto">
+          <p className="text-center"> Nothing to show yet! </p>
+        </div>
       </div>
     </ErrorBoundary>
   )
-};
+}
 
 export default App
--- hydroweb/settings/prod.py
+++ hydroweb/settings/prod.py
@@ -1,3 +1,5 @@
 """Production settings for hydroweb project on Django 3.2.6."""
 
 from .base import *
+
+PRODUCTION = True

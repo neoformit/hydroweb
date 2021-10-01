ce9bb3a9f00630711b5576c1216da66f986335d0
Cameron Hyde <c.hyde@qcif.edu.au>
2021-10-01 17:22:40 +1000

    Add main component and status bar

---
+++ dash/static/dash/js/app/src/components/main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import StatusBar from './StatusBar'
+
+const Main = (props) => {
+
+  return (
+    <div className="main">
+      <StatusBar />
+    </div>
+  )
+
+}
+
+export default Main
--- dash/static/dash/js/app/webpack.config.prod.js
+++ dash/static/dash/js/app/webpack.config.prod.js
@@ -18,6 +18,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname + '/src'),
+      path.resolve(__dirname + '/node_modules'),
+    ],
   },
   output: {
     path: path.resolve(__dirname, './dist'),

508ac723aea4c4ae58ac7dcc557803f1ae8a04ae
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2021-12-31 09:07:40 +1000

    Fix time zone

--- hydroweb/settings/base.py
+++ hydroweb/settings/base.py
@@ -95,15 +95,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Australia/Brisbane'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/

25eb86fdc360851fd65d14ece75b99162ebacf5e
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2022-01-12 21:49:51 +1000

    Fix logging to include hydropi

--- hydroweb/settings/log.py
+++ hydroweb/settings/log.py
@@ -13,7 +13,15 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'standard': {
-            'format': '%(levelname)5s | %(asctime)s | %(module)12s: %(message)s',
+            'format': (
+                '%(levelname).4s | %(asctime)s | %(module)-12s| %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+            'style': "%",
+        },
+        'hydropi': {
+            'format': (
+                '%(levelname).4s | %(asctime)s'
+                ' | [hydropi] %(module)-12s| %(message)s'),
             'datefmt': '%Y-%m-%d %H:%M:%S',
             'style': "%",
         },
@@ -28,10 +36,25 @@ LOGGING = {
             'mode': 'a',
             'formatter': 'standard',
         },
+        'hydropi_file': {
+            'delay': True,
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 1000000,  # 1MB ~ 20k rows
+            'backupCount': 5,
+            'filename': os.path.join(LOG_ROOT, 'hydropi.log'),
+            'mode': 'a',
+            'formatter': 'standard',
+        },
         'console': {
             'class': 'logging.StreamHandler',
             'level': 'INFO',
             'formatter': 'standard',
+        },
+        'hydropi_console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'hydropi',
         },
     },
     'loggers': {
@@ -39,6 +62,11 @@ LOGGING = {
             'level': 'DEBUG',
             'handlers': ['console', 'file'],
             'propagate': True,
+        },
+        'hydropi': {
+            'level': 'DEBUG',
+            'handlers': ['hydropi_console', 'hydropi_file'],
+            'propagate': False,
         },
     }
 }

1f413fb79b1333c4f279a0562a707f3edcaaa2f7
Cameron Hyde <chyde@neoformit.com>
2022-10-29 10:50:02 +1000

    Update service control endpoint to use pause feature

--- api/endpoints/controllers.py
+++ api/endpoints/controllers.py
@@ -53,26 +53,22 @@ class ControllerView(View):
 class ServiceView(View):
     """Allow management of the hydropi service."""
 
+    def get(self, request):
+        """Return pause state."""
+        return JsonResponse({
+            'state': handlers.controllers.is_paused(),
+        })
+
+
     @method_decorator(staff_member_required)
     def post(self, request):
         """Handle a service management request."""
         r = None
         try:
             data = get_json_payload(request)
-            args = [
-                'sudo',
-                'service',
-                'hydropi',
-                data['action'],
-            ]
-            r = subprocess.run(args, check=True, capture_output=True)
+            handlers.controllers.set_pause()
         except Exception as exc:
-            logger.error(
-                "Error encountered handling service management request with"
-                f' args: "{args}"')
+            logger.error("Error encountered handling pause request:")
             logger.error(f"Exception: {str(exc)}")
-            if r:
-                logger.error(f"STDERR:\n{r.stderr}")
-                return HttpResponse(r.stderr, status=500)
             return HttpResponse(str(exc), status=500)
         return HttpResponse('OK', status=201)

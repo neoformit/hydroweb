af7b454b0c80818732e79e973562286bf0817484
Cameron Hyde <chyde@neoformit.com>
2022-10-29 10:27:30 +1000

    Enable manifest static file management for cache busting

--- hydroweb/settings/base.py
+++ hydroweb/settings/base.py
@@ -111,6 +111,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'hydroweb/static'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
